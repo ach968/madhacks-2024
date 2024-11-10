421e11e1f625681a28ab109baae846084568c867
eshen7 <edisonshen314@gmail.com>
2024-11-09 19:48:05 -0600

    breakeven and win pct

--- backend/services/simulate.py
+++ backend/services/simulate.py
@@ -1,9 +1,10 @@
+import math
 from typing import List
 from treys import Deck, Evaluator, Card
 import matplotlib.pyplot as plt
 from multiprocessing import Pool, cpu_count
 import numpy as np
-from scipy.stats import skewnorm, binom
+from scipy.stats import norm
 
 class Hand:
     def __init__(self, cards):
@@ -29,6 +30,8 @@ def single_trial(player_hand: Hand, num_opponents: int, stage: int, board: List[
         win = True
         for opponent in opponents:
             opponent_score = evaluator.evaluate(opponent.cards, current_board)
+            if (opponent_score < 1000):
+                break
             if opponent_score < player_score:
                 win = False
                 break
@@ -48,20 +51,37 @@ def simulate(player_hand: Hand, num_opponents: int, stage: int, board: List[int]
     
     return results
 
+def breakeven(total_pot: int, player_in: int, mean: float, sd: float):
+    win_chance = 1.0 * player_in / total_pot
+    z_score = (win_chance - mean) / sd
+    percent =  1 -norm.cdf(z_score)
+    return percent
+
 def main():
-    player_hand = Hand([Card.new("As"), Card.new("2s")])
-    board = [Card.new("Ac"), Card.new("Ad"), Card.new("8d")]
-    num_opponents = 3
-    stage = 3
+    player_hand = Hand([Card.new("Ad"), Card.new("Kd")])
+    board = []
+    num_opponents = 6
+    stage = 5
     trials = 100
     n = 5000
 
     wins = simulate(player_hand, num_opponents, stage, board, trials, n)
-    data = np.array(wins)
+    data = np.array(wins) / trials
     mean = np.mean(data)
-    sd = np.std(data)
+    sd = np.std(data) * math.sqrt(trials)
 
     print(mean, sd)
+
+    per_player = 100
+
+    total_pot = (num_opponents + 1) * per_player
+    player_in = per_player
+
+    perc = breakeven(total_pot, player_in, mean, sd) * 100
+
+    print('You have a ', mean * 100, ' percent chance to win')
+
+    print('You have a ', perc, ' percent chance to at least break even')
 
     plt.hist(wins, bins=np.arange(min(wins), max(wins) + 2) - 0.5, edgecolor='black')
     plt.xlabel('# of wins')
